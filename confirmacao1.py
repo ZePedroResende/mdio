ff9f504a16f2e080254d8a4a6df8a0397a1386ea
Tânia Silva <tdosilva@gmail.com>
2017-10-18 00:08:50 +0100

    Corrigida parte 1

--- confirmacao1.py
+++ confirmacao1.py
@@ -1,25 +1,25 @@
-X1=4
-Y1=4
-X2=4
-Y2=3
-X3=5
-Y3=1
+X1=60
+Y1=60
+X2=32
+Y2=24
+X3=85
+Y3=17
 X4=0
 Y4=0
-X5=2
-Y5=2
-X6=6
-Y6=6
-X7=2
-Y7=5
-X8=7
-Y8=1
-X9=2
-Y9=2
+X5=8
+Y5=8
+X6=24
+Y6=24
+X7=36
+Y7=90
+X8=56
+Y8=8
+X9=8
+Y9=8
 X10=0
 Y10=0
-X11=2
-Y11=2
+X11=10
+Y11=10
 XD1=6
 YD1=4
 XD2=8
@@ -33,91 +33,89 @@ def Somatorio():
     maxj = 4
     i=0
     total = 0
-    while(i<= maxi):
+    while (i<=maxi):
         j=0
         while(j<=maxj):
             total += NumeroDeDeslocacoes[i][j]* ( abs(eval("XD" +str(i+1)) -
                                                       Coordenadas[j][0]) +
                                                  abs(eval("YD" + str(i+1)) -
                                                      Coordenadas[j][1]))
-            j+= 1
+            j += 1
         i+=1
     return total
 
 def funcaoObjetivo():
-    return 5 *(abs(XD1 - XD2)+abs(YD1 - YD2)) + Somatorio()
+    return 5*(abs(XD1-XD2)+abs(YD1-YD2)) + Somatorio()
 
-confirmacao= [
-15*X1 + 15*Y1 + 8*X2 + 8*Y2 + 17 *X3 + 17*Y3 + 58*X4 + 58*Y4 + 4*X5 + 4*Y5 +
-    4*X6 + 4*Y6 + 18*X7 + 18*Y7 + 8*X8 + 8*Y8 + 4*X9 + 4*Y9 + 82*X10 + 82*Y10 +
-    5*X11 + 5*Y11 == funcaoObjetivo()
+
+confirmacao = [
+X1 + Y1 + X2 + Y2 + X3 + Y3 + X4 + Y4 + X5 + Y5 + X6 + Y6+ X7 + Y7 + X8 + Y8 + X9 + Y9 + X10 + Y10 + X11 + Y11 == funcaoObjetivo()
 ,
-X1 >= XD1 - 2
-,X1 >=  - XD1 + 2
-,Y1 >= YD1 - 8
-,Y1 >=  - YD1 + 8
+X1 >= 15*XD1 - 30
+,X1 >=( -15)*XD1 + 30
+,Y1 >= 15*YD1 - 120
+,Y1 >= (-15)*YD1 + 120
 
-,X2 >= XD1 - 10
-,X2 >=  - XD1 + 10
-,Y2 >= YD1 - 7
-,Y2 >= -YD1 + 7
+,X2 >= 8*XD1 - 80
+,X2 >= (-8)*XD1 + 80
+,Y2 >= 8*YD1 -56
+,Y2 >= (-8)*YD1 + 56
 
-,X3 >= XD1 - 1
-,X3 >=  - XD1 + 1
-,Y3 >= YD1 - 3
-,Y3 >=  - YD1 + 3
+,X3 >= 17*XD1 - 17
+,X3 >=  (-17)*XD1 + 17
+,Y3 >= 17*YD1 - 51
+,Y3 >=  (-17)*YD1 + 51
 
-,X4 >= XD1 - 6
-,X4 >=  - XD1 + 6
-,Y4 >= YD1 - 4
-,Y4 >=  - YD1 + 4
+,X4 >= 58*XD1 - 348
+,X4 >=  (-58)*XD1 + 348
+,Y4 >= 58*YD1 - 232
+,Y4 >=  (-58)*YD1 + 232
 
-,X5 >= XD1 - 8
-,X5 >=  - XD1 + 8
-,Y5 >= YD1 - 2
-,Y5 >=  - YD1 + 2
+,X5 >= 4*XD1 - 32
+,X5 >=  (-4)*XD1 + 32
+,Y5 >= 4*YD1 - 8
+,Y5 >=  (-4)*YD1 + 8
 
-,X6 >= XD2 - 2
-,X6 >=  - XD2 + 2
-,Y6 >= YD2 - 8
-,Y6 >=  - YD2 + 8
+,X6 >= 4*XD2 - 8
+,X6 >=  (-4)*XD2 + 8
+,Y6 >= 4*YD2 - 32
+,Y6 >=  (-4)*YD2 + 32
 
-,X7 >= XD2 - 10
-,X7 >=  - XD2 + 10
-,Y7 >= YD2 - 7
-,Y7 >=  - YD2 + 7
+,X7 >= 18*XD2 - 180
+,X7 >=  (-18)*XD2 + 180
+,Y7 >= 18*YD2 - 126
+,Y7 >=  (-18)*YD2 + 126
 
-,X8 >= XD2 - 1
-,X8 >=  - XD2 + 1
-,Y8 >= YD2 - 3
-,Y8 >=  - YD2 + 3
+,X8 >= 8*XD2 - 8
+,X8 >=  (-8)*XD2 + 8
+,Y8 >= 8*YD2 - 24
+,Y8 >=  (-8)*YD2 + 24
 
-,X9 >= XD2 - 6
-,X9 >=  - XD2 + 6
-,Y9 >= YD2 - 4
-,Y9 >=  - YD2 + 4
+,X9 >= 4*XD2 - 24
+,X9 >=  (-4)*XD2 + 24
+,Y9 >= 4*YD2 - 16
+,Y9 >=  (-4)*YD2 + 16
 
-,X10 >= XD2 - 8
-,X10 >=  - XD2 + 8
-,Y10 >= YD2 - 2
-,Y10 >=  - YD2 + 2
+,X10 >= 82*XD2 - 656
+,X10 >=  (-82)*XD2 + 656
+,Y10 >= 82*YD2 - 164
+,Y10 >=  (-82)*YD2 + 164
 
-,X11 >= XD1 - XD2
-,X11 >=  - XD1 + XD2
-,Y11 >= YD1 - YD2
-,Y11 >=  - YD1 + YD2
+,X11 >= 5*XD1 - 5*XD2
+,X11 >=  (-5)*XD1 + 5*XD2
+,Y11 >= 5*YD1 - 5*YD2
+,Y11 >=  (-5)*YD1 + 5*YD2
 ]
 
 
 def printDesenvolvimento():
     lista = ["X","Y"]
-    N = [item for sublist in NumeroDeDeslocacoes for item in sublist]
     lol = ""
-    for a in range(1,11):
+    for a in range(1,12):
         for b in lista :
-            lol += str(N[a-1]) + "*" + str(eval (b + str(a))) + "+ "
-    lol += "5*"+ str(X11) + " + 5*" + str(Y11)
-    lol += ("==" + str(funcaoObjetivo()) )
+            lol += str(eval (b + str(a)))+ " + "
+    lol += str(X11) + " + "+ str(Y11)
+    lol += (" == " + str(funcaoObjetivo()) )
     print(lol)
 
 
